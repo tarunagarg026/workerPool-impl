4d38dd5372c63ecc751060b9f272587f0e62d113
Taruna Garg <taruna@Tarunas-MacBook-Pro-2.local>
2022-03-16 14:36:17 +0530

    feat: test cases

--- workerPool/pool_test.go
+++ workerPool/pool_test.go
@@ -40,7 +40,7 @@ func TestExecuteReq_EmptyName(t *testing.T) {
 
 func TestExecuteReq_WrongDelay(t *testing.T) {
 	pool := GetPoolConfig(5, 1, 10)
-	var jsonStr = []byte(`{"name":"", "delay": 20}`)
+	var jsonStr = []byte(`{"name":"test", "delay": 20}`)
 	req, _ := http.NewRequest("POST", "localhost:8000/work", bytes.NewBuffer(jsonStr))
 	req.Header.Set("Content-Type", "application/json")
 	response := executeRequest(req)
@@ -61,7 +61,17 @@ func TestExecuteReq_WrongMethod(t *testing.T) {
 func TestExecuteReq_WrongDelayFormat(t *testing.T) {
 	pool := GetPoolConfig(5, 1, 10)
 	var jsonStr = []byte(`{"name":"test", "delay": "20""}`)
-	req, _ := http.NewRequest("PUT", "localhost:8000/work", bytes.NewBuffer(jsonStr))
+	req, _ := http.NewRequest("POST", "localhost:8000/work", bytes.NewBuffer(jsonStr))
+	req.Header.Set("Content-Type", "application/json")
+	response := executeRequest(req)
+	parseReq(response, req, pool)
+	checkResponseCode(t, http.StatusBadRequest, response.Code)
+}
+
+func TestExecuteReq_WrongNameFormat(t *testing.T) {
+	pool := GetPoolConfig(5, 1, 10)
+	var jsonStr = []byte(`{"name": 22, "delay": 1"}`)
+	req, _ := http.NewRequest("POST", "localhost:8000/work", bytes.NewBuffer(jsonStr))
 	req.Header.Set("Content-Type", "application/json")
 	response := executeRequest(req)
 	parseReq(response, req, pool)
